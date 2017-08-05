43240f73cdd8d32c7d44d861516023da0b6f510b
Robert <RobertIndie@outlook.com>
2017-08-05 16:56:45 +0800

    Add draw project

---
+++ Test01/Draw.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test01
+{
+    class Draw
+    {
+        public static void Main(Bitmap bmp)
+        {
+            #region 让屏幕变白
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                    bmp.SetPixel(i, j, Color.FromArgb(255, 255, 255));
+            }
+            #endregion
+            #region 不正经画图
+            for(int i = 0;i<bmp.Width;i++)
+            {
+                for(int j = 0;j<bmp.Height;j++)
+                {
+                    //bmp.SetPixel(i, j, Color.FromArgb(i, j, i+j));
+                    //bmp.SetPixel(i, j, Color.FromArgb(i, j, i - j));
+                    //bmp.SetPixel(i, j, Color.FromArgb(i, j, i * j));
+                    //bmp.SetPixel(i, j, Color.FromArgb(i, j, i / (j==0?1:j)));
+                    //bmp.SetPixel(i, j, Color.FromArgb(i,i,i));
+                    //bmp.SetPixel(i, j, Color.FromArgb(i+Random.Next(), i + Random.Next(), i + Random.Next()));
+                }
+            }
+            #endregion
+            #region 正经画图
+            //定义个正方形
+            Rect rect;
+            rect.width = rect.height = 100;
+            rect.x = rect.y = 50;
+            //画个正方形
+            for (int i = rect.x; i < rect.x+(rect.width > bmp.Width ? bmp.Width : rect.width); i++)
+            {
+                for (int j = rect.y; j < rect.y+(rect.height > bmp.Height ? bmp.Height : rect.height); j++)
+                {
+                    bmp.SetPixel(i, j, Color.FromArgb(32, 223, 245));
+                }
+            }
+            rect.x += 200;
+            DrawRect(bmp,rect);
+            Rect mouth = new Rect() { x = 100, y = 250, width = 200, height = 40 };
+            DrawRect(bmp,mouth);
+            #endregion
+        }
+        static void DrawRect(Bitmap bmp, Rect rect)
+        {
+            for (int i = rect.x; i < rect.x+(rect.width > bmp.Width ? bmp.Width : rect.width); i++)
+            {
+                for (int j = rect.y; j < rect.y+(rect.height > bmp.Height ? bmp.Height : rect.height); j++)
+                {
+                    bmp.SetPixel(i, j, Color.FromArgb(32, 223, 245));
+                }
+            }
+        }
+        struct Rect
+        {
+            public int width;
+            public int height;
+            public int x;
+            public int y;
+        }
+    }
+}
---
+++ Test01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test01
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+    class Color
+    {
+        public static System.Drawing.Color FromArgb(byte r,byte g,byte b)
+        {
+            return System.Drawing.Color.FromArgb(r, g, b);
+        }
+        public static System.Drawing.Color FromArgb(int r, int g, int b)
+        {
+            return FromArgb(
+                    (byte)r,
+                    (byte)g,
+                    (byte)b
+                );
+        }
+    }
+    public class Random
+    {
+        static System.Random random = new System.Random();
+        public static int Next()
+        {
+            return random.Next(1, 70);
+        }
+    }
+}
